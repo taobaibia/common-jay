f3fdb2c26633a334a382058aca8eb71c7755512b
申智杰 <312838735@qq.com>
2025-02-27 16:56:10 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# common-jay
+ inte
---
+++ jay-common-bops/src/main/java/org/common/bops/CommonBopsWebApplication.java
@@ -0,0 +1,53 @@
+package org.common.bops;
+
+import java.util.TimeZone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author Jay
+ * @version V1.0
+ * @Package org.common.jay
+ * @date ${DATE} ${TIME}
+ */
+@SpringBootApplication
+@ImportResource({ "classpath*:/**/spring/*.xml" })
+@EnableFeignClients(basePackages = { "com.*" })
+@EnableAsync
+@RestController
+@ComponentScan(basePackages = { "com.*", "com.*.*" })
+public class CommonBopsWebApplication {
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT+8"));
+        SpringApplication.run(CommonBopsWebApplication.class, args);
+        System.out.println(
+                " 																																                                                       \n"
+                        + " ██████╗ ██████╗ ███╗   ███╗███╗   ███╗ ██████╗ ███╗   ██╗      ██████╗  ██████╗ ██████╗ ███████╗\n"
+                        + "██╔════╝██╔═══██╗████╗ ████║████╗ ████║██╔═══██╗████╗  ██║      ██╔══██╗██╔═══██╗██╔══██╗██╔════╝\n"
+                        + "██║     ██║   ██║██╔████╔██║██╔████╔██║██║   ██║██╔██╗ ██║█████╗██████╔╝██║   ██║██████╔╝███████╗\n"
+                        + "██║     ██║   ██║██║╚██╔╝██║██║╚██╔╝██║██║   ██║██║╚██╗██║╚════╝██╔══██╗██║   ██║██╔═══╝ ╚════██║\n"
+                        + "╚██████╗╚██████╔╝██║ ╚═╝ ██║██║ ╚═╝ ██║╚██████╔╝██║ ╚████║      ██████╔╝╚██████╔╝██║     ███████║\n"
+                        + " ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚═╝     ╚═╝ ╚═════╝ ╚═╝  ╚═══╝      ╚═════╝  ╚═════╝ ╚═╝     ╚══════╝\n");
+    }
+
+
+//    @Bean
+//    public ServletWebServerFactory webServerFactory() {
+//        TomcatServletWebServerFactory fa = new TomcatServletWebServerFactory();
+//        // 解决url中含特殊字符报错问题
+//        fa.addConnectorCustomizers(connector -> {
+//            connector.setProperty("relaxedPathChars", "\"<>[\\]^`{|}");
+//            connector.setProperty("relaxedQueryChars", "\"<>[\\]^`{|}");
+//        });
+//        return fa;
+//    }
+
+
+}
